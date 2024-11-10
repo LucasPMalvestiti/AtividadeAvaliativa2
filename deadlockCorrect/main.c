bae50a92e80a20ca71e2590905c55c7c8363d746
ItsMalvs23 <104511261+ItsMalvs23@users.noreply.github.com>
2024-11-10 12:50:22 -0300

    DONE

--- deadlockCorrect/main.c
+++ deadlockCorrect/main.c
@@ -3,24 +3,20 @@
 #include <pthread.h>
 #include <unistd.h>
 
-// Declaração dos mutexes globais
 pthread_mutex_t mutex1;
 pthread_mutex_t mutex2;
 
 void* thread_func1(void* arg) {
-    // Tenta bloquear os mutexes em ordem consistente (mutex1 depois mutex2)
     pthread_mutex_lock(&mutex1);
     printf("Thread 1: bloqueou mutex1\n");
 
-    sleep(1); // Simula uma operação que leva tempo
+    sleep(1);
 
     pthread_mutex_lock(&mutex2);
     printf("Thread 1: bloqueou mutex2\n");
 
-    // Realiza operações críticas
     printf("Thread 1: realizando operações críticas\n");
 
-    // Libera os mutexes na ordem inversa
     pthread_mutex_unlock(&mutex2);
     pthread_mutex_unlock(&mutex1);
 
@@ -28,19 +24,16 @@ void* thread_func1(void* arg) {
 }
 
 void* thread_func2(void* arg) {
-    // Tenta bloquear os mutexes em ordem consistente (mutex1 depois mutex2)
     pthread_mutex_lock(&mutex1);
     printf("Thread 2: bloqueou mutex1\n");
 
-    sleep(1); // Simula uma operação que leva tempo
+    sleep(1);
 
     pthread_mutex_lock(&mutex2);
     printf("Thread 2: bloqueou mutex2\n");
 
-    // Realiza operações críticas
     printf("Thread 2: realizando operações críticas\n");
 
-    // Libera os mutexes na ordem inversa
     pthread_mutex_unlock(&mutex2);
     pthread_mutex_unlock(&mutex1);
 
@@ -48,20 +41,16 @@ void* thread_func2(void* arg) {
 }
 
 int main() {
-    // Inicializa os mutexes
     pthread_mutex_init(&mutex1, NULL);
     pthread_mutex_init(&mutex2, NULL);
 
-    // Cria as threads
     pthread_t thread1, thread2;
     pthread_create(&thread1, NULL, thread_func1, NULL);
     pthread_create(&thread2, NULL, thread_func2, NULL);
 
-    // Aguarda a finalização das threads
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
 
-    // Destroi os mutexes
     pthread_mutex_destroy(&mutex1);
     pthread_mutex_destroy(&mutex2);
 
